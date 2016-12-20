6f3ccb92113c15d82786418822e7f34f2d4408c8
Tejen Shrestha <shrestha_tejen@bah.com>
2016-12-20 15:41:09 -0500

    Local uuid function.

--- src/js/threesy-line.js
+++ src/js/threesy-line.js
@@ -15,7 +15,7 @@ const defaultYAccessor = "y";
  * http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
  */
 const uuid = () => {
-    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
